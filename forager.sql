d86ad221554d66c6a647fb1930fd935fd2759e68
Lee Hall <shall4@spsu.edu>
2012-10-20 22:25:19 -0400

    Fixed some dependencies

--- forager.sql
+++ forager.sql
@@ -12,31 +12,52 @@
 BEGIN;
 
 SET ROLE forager;
+
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
 	user_id 	SERIAL PRIMARY KEY,
 	user_name	varchar UNIQUE NOT NULL,
 	password	varchar	
 );
 
+COMMENT ON TABLE users IS 'Rudimentary user login table.';
+
+DROP TABLE IF EXISTS scans CASCADE;
 CREATE TABLE scans (
 	scan_id 	SERIAL PRIMARY KEY,
 	start_time	timestamp,
 	end_time	timestamp	
 );
+COMMENT ON TABLE scans IS 'List of scans, referenced by resources';
 
+DROP TABLE IF EXISTS resources CASCADE;
 CREATE TABLE resources (
 	resource_id		SERIAL PRIMARY KEY,
-	url				varchar(),
-	parent_request	integer REFERENCES resources(resource_id),
+	scan_id			integer REFERENCES scans(scan_id)
+		ON DELETE CASCADE,
+	url				varchar,
+	parent_id		integer REFERENCES resources(resource_id)
+		ON DELETE CASCADE,
 	start_date		timestamp,	
 	response_time	interval,
-	http_response	integer
+	http_response	integer,
+	UNIQUE (scan_id,url)
 );
 
+COMMENT ON TABLE resources IS 'List of pages retrieved. This forms a tree'
+	' for each scan, rooted at the node with a null parent_id. This is a'
+	' spanning tree of the graph in resource_children.'; 
+
+DROP TABLE IF EXISTS resource_children CASCADE;
 CREATE TABLE resource_children (
-	resource_id		integer REFERENCES resources(resource_id),
+	resource_id		integer REFERENCES resources(resource_id)
+		ON DELETE CASCADE,
 	child_id		integer REFERENCES resources(resource_id)
+		ON DELETE CASCADE
 );
+
+COMMENT ON TABLE resource_children IS 'Edge set of the graph of the website.'
+	' Edges in the tree specified by parent_id also exist here.';
 
 COMMIT;
 
