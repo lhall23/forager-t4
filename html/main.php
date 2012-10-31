7e0c7332d09ff986613d1b718473fee1e512a3b2
tcouture127 <tcouture127@gmail.com>
2012-10-30 21:19:01 -0400

    Main Page Push!

---
+++ html/main.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type"
+ content="text/html; charset=iso-8859-1">
+  <title>Your Company</title>
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="container">
+<div id="header"> <img src="images/logo.jpg" alt="" id="logo">
+<h1 id="logo-text">Forager</h1>
+</div>
+<div id="nav">
+<ul>
+  <li><a href="main">Home</a></li>
+  <li><a href="scan">Start a Scan</a></li>
+  <li><a href="<?php echo "Reports.php"; ?>">View Reports</a></li>
+  <li><a href="compare">Compare Reports</a></li>
+  <li><a href="extra">Extra</a></li>
+  <li style="border-right: medium none;"><a href="#">Links</a></li>
+</ul>
+</div>
+<div id="site-content">
+<div id="col-left">
+<h1 class="h-text-1">WELCOME</h1>
+<p class="text-1"><strong>Lorem Ipsum is simply</strong> dummy text of
+the printing and typesetting industry. Lorem Ipsum has been the
+industry's standard dummy text ever since the 1500s, when an unknown
+printer took a galley of type and scrambled.</p>
+<ul class="list-1">
+  <li>Lorem Ipsum as their default model text</li>
+  <li>There are many variations of passages</li>
+  <li>Contrary to popular belief, Lorem Ipsum is not</li>
+  <li>Many desktop publishing packages and web page</li>
+  <li>There are many variations of passages</li>
+</ul>
+<p class="text-1">Lorem Ipsum is simply dummy text of the printing and
+typesetting industry. Lorem Ipsum has been the industry's standard
+dummy text ever since the 1500s, when an unknown printer took a galley
+of type and scrambled it to make a type specimen book. It has survived
+not only five centuries, but also the leap into electronic typesetting,
+remaining essentially unchanged. It was popularised in the 1960s with
+the release of Letraset sheets containing Lorem Ipsum passages,</p>
+<p class="border-1">&nbsp;</p>
+<h2 class="h-text-2">About us</h2>
+<p class="text-1">There are many variations of passages of Lorem Ipsum
+available, but the majority have suffered alteration in some form, by
+injected humour, or randomised words which don't look even slightly
+believable. If you are going to use a passage of Lorem Ipsum, you need
+to be sure there isn't anything embarrassing hidden. There are many
+variations of passages of Lorem Ipsum available, but the majority have
+suffered.</p>
+<p class="text-1">All the Lorem Ipsum generators on the Internet tend
+to repeat predefined chunks as necessary, making this the first true
+generator on the Internet. It uses a dictionary of over 200 Latin
+words, combined with a handful of model sentence structures, to
+generate Lorem Ipsum which looks reasonable. </p>
+</div>
+<div id="col-right">
+<div style="padding: 30px 10px 10px;">
+<h2 class="h-text-2">Latest News</h2>
+<h3 class="h-text-3">Forager Version 1.0</h3>
+<p class="text-2">Version 1.0 has been released. At the moment, forager is capable of searching the websites and populating a report that lists off the erros encountered.</p>
+
+</div>
+<div>&nbsp;</div>
+<div style="padding: 5px 10px;">
+<h2 class="h-text-2">Contact Info</h2>
+</div>
+<div
+ style="padding: 5px 10px 15px; background: rgb(216, 214, 215) none repeat scroll 0%; -moz-background-clip: initial; -moz-background-origin: initial; -moz-background-inline-policy: initial;">
+<p class="text-2"> 00/00 Lorem Ipsum is simply dummy text of the
+printing and typesetting.<br>
+<br>
+E.mail: abc@Lorem Ipsum<br>
+<br>
+Fax: 000.000.0000<br>
+<br>
+Phone: 000.000.0000/<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+000.000.0000 </p>
+</div>
+</div>
+</div>
+<div id="footer">
+<p>@ Copyright 2010. Designed by <a target="_blank"
+ href="http://www.htmltemplates.net/">HTML Templates</a></p>
+<ul class="footer-nav">
+  <li><a href="#">Home</a></li>
+  <li><a href="#">About us</a></li>
+  <li><a href="#">Recent articles</a></li>
+  <li><a href="#">Email</a></li>
+  <li><a href="#">Resources</a></li>
+  <li><a href="#">Links</a></li>
+</ul>
+</div>
+</div>
+</body>
+</html>
