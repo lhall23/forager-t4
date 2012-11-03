d2396396789bfbaf7d81ec10ee6505c16676e2e8
tcouture127 <tcouture127@gmail.com>
2012-11-03 17:41:49 -0400

    Preview Scans

--- html/Reports.php
+++ html/Reports.php
@@ -42,11 +42,23 @@ require_once('include/conf.php');
 
 <?php
 
-$query = "SELECT scan_id, start_time, end_time, end_time - start_time FROM scans"; 
+$query = "SELECT scan_id, start_time, end_time, end_time - start_time as elapsed_time FROM scans"; 
 $scans = pg_query($conn, $query);
-$results = pg_fetch_all($scans);
 
-$js_array = json_encode($results);
+$js_array = "[";
+
+while($results = pg_fetch_array($scans))
+{
+$js_array += "[";
+$js_array += $results['scan_id'];
+$js_array += $results['start_time'];
+$js_array += $results['end_time'];
+$js_array += $results['elapsed_time'];
+$js_array += "]";
+}
+
+
+$js_array += "]";
 
 ?>
 
