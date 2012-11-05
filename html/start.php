36e89876ef54aafea99107e244b4ee7397d9c185
Lee Hall <shall4@spsu.edu>
2012-11-04 19:30:47 -0500

    Check current status
    Check to see if a crawler is already running before starting

--- html/start.php
+++ html/start.php
@@ -3,9 +3,9 @@
  * start.php
  * -Lee Hall Sat 03 Nov 2012 06:50:57 PM EDT
  */
-require_once('include/config.php');
+require_once('include/conf.php');
 require_once('include/session.php');
-exec("/usr/local/src/forager/bin/crawler.py");
+exec("/usr/local/src/forager/bin/crawler.py&");
 header("Location: main.php");
 
 ?>
