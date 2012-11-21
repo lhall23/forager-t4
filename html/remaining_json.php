63b29575f887a7bda3a28e609a86770506ad2e16
Lee Hall <lhall@newfields.com>
2012-11-21 05:38:46 -0500

    JSON for progress.
    remaining_json.php reports number of records thus far in the current
    scan, as well as the start date.

---
+++ html/remaining_json.php
@@ -0,0 +1,23 @@
+<?php
+/* 
+ * remaining_json.php
+ * -Lee Hall Wed 21 Nov 2012 05:27:46 AM EST
+ */
+
+require_once("include/conf.php");
+require_once("include/session.php");
+
+if(array_key_exists("scan_id", $_GET)){
+    $scan_id=$_GET['scan_id'];
+} else {
+    die("Please specify a scan_id.");
+}
+
+$sql="SELECT DISTINCT start_time, 
+        COUNT(*) OVER (PARTITION BY scan_id) AS total
+    FROM resources JOIN scans USING(scan_id)
+    WHERE scan_id=$1";
+$res=pg_query_params($conn, $sql, array($scan_id));
+$row=pg_fetch_assoc($res);
+echo json_encode($row);
+?>
