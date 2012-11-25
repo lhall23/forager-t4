5f244d2a2ad90ae446329788d5dce60bebae1152
Lee Hall <shall4@spsu.edu>
2012-11-25 18:43:40 -0500

    Fixed comparison query

--- html/compare_json.php
+++ html/compare_json.php
@@ -2,28 +2,33 @@
 require_once("include/conf.php");
 require_once("include/json_session.php");
 
-if(array_key_exists('compare_one',$_GET)) {
-	$scan_id1 = $_GET['compare_one'];
+if(array_key_exists('firstId',$_GET)) {
+	$scan_id1 = $_GET['firstId'];
 } else {
 	die("Please specify a scan_id");
 }
 
-if(array_key_exists('scan_id',$_GET)) {
-	$scan_id2 = $_GET['scan_id'];
+if(array_key_exists('secondId',$_GET)) {
+	$scan_id2 = $_GET['secondId'];
 } else {
 	die("Please specify a scan_id");
 }
 
-$query = "SELECT  url, 
-        COALESCE(response_name, resource1.http_response || ' (Unknown Response)') 
-            AS http_response1
-		COALESCE(response_name, http_response || ' (Unknown Response)') 
+$query = "SELECT url, 
+        COALESCE(r1.response_name, resource1.http_response ||
+                ' (Unknown Response)')
+            AS http_response1,
+        COALESCE(r2.response_name, resource2.http_response ||
+                ' (Unknown Response)')
             AS http_response2
-    FROM resources AS resource1
-	OUTER JOIN resources AS resource2
-    LEFT JOIN http_responses AS r1 ON(resource1.http_response=r1.http_responce) AS  
-    LEFT JOIN http_responses AS r2 ON(resource1.http_response=r2.http_responce) AS  
-	WHERE resource1.scan_id = $1 AND resource2.scan_id=$2"; 
+    FROM (SELECT * FROM resources WHERE scan_id=$1) AS resource1
+    FULL JOIN (SELECT * FROM resources WHERE scan_id=$2) AS resource2
+        USING(url)
+    LEFT JOIN http_responses AS r1 ON(resource1.http_response=r1.http_response)
+    LEFT JOIN http_responses AS r2 ON(resource2.http_response=r2.http_response)
+    WHERE resource1.http_response != resource2.http_response OR 
+        resource1.http_response IS NULL OR resource2.http_response IS NULL;";
+		//AND resource1.http_response != resource2.http_response"; 
 $reports = pg_query_params($conn, $query,array($scan_id1, $scan_id2));
 
 $columns=array(
